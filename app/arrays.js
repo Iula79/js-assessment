c9d09aed39a98b8ec81fb132f9ceb92942cf1ccd
Giulia <giulialake@gmail.com>
2017-03-01 00:32:06 -0500

    added array solutions

--- app/arrays.js
+++ app/arrays.js
@@ -1,59 +1,104 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
-  indexOf: function(arr, item) {
+    indexOf: function(arr, item) {
+        return arr.indexOf(item);
+    },
 
-  },
+    sum: function(arr) {
+        return arr.reduce((a, b) => a + b);
+    },
 
-  sum: function(arr) {
+    remove: function(arr, item) {
 
-  },
+        arr.forEach(function(){
+                arr.splice(arr.indexOf(item), 1);
+        });
+        return arr
+    },
 
-  remove: function(arr, item) {
+    removeWithoutCopy: function(arr, item) {
 
-  },
+        var arrLength = arr.length - 1;
+        for (i = arrLength; i >= 0; i--) {
+            if (arr[i] == item) {
+                arr.splice(i, 1);
+            }          
+        }
+        return arr
+    },
 
-  removeWithoutCopy: function(arr, item) {
+    append: function(arr, item) {
+         arr.push(item)
+         return arr
+    },
 
-  },
+    truncate: function(arr) {
+        arr.pop()
+         return arr
+    },
 
-  append: function(arr, item) {
+    prepend: function(arr, item) {
+        arr.unshift(item)
+        return arr
+    },
 
-  },
+    curtail: function(arr) {
+        arr.shift()
+        return arr
+    },
 
-  truncate: function(arr) {
+    concat: function(arr1, arr2) {
+         var newArr= arr1.concat(arr2)
+         return newArr
+    },
 
-  },
+    insert: function(arr, item, index) {
+        arr.splice(index, 0, item);
+        return arr
 
-  prepend: function(arr, item) {
+    },
 
-  },
+    count: function(arr, item) {
+        var count = 0
+        for (i=0; i<arr.length; i++){
+            if (arr[i] == item){
+                count ++
+            }
+        }
+        return count
+    },
 
-  curtail: function(arr) {
+    duplicates: function(arr) {
+        arr.sort();
+        var result = [];
+        var count =0;
+        for(i=0; i<arr.length; i++){
+            if(arr[i]== arr[i-1]&& count!=arr[i]){
+                result.push(arr[i])
+                count = arr[i];
+            }else if ( arr[i]== arr[i-1]&& count==arr[i]){
+                continue;
+            } 
+        }
+        return result
+    },
 
-  },
+    square: function(arr) {
+        newArr = []
+        arr.forEach(i=>{i=i * i;
+            newArr.push(i)
+        });
+        return newArr
+    },
 
-  concat: function(arr1, arr2) {
-
-  },
-
-  insert: function(arr, item, index) {
-
-  },
-
-  count: function(arr, item) {
-
-  },
-
-  duplicates: function(arr) {
-
-  },
-
-  square: function(arr) {
-
-  },
-
-  findAllOccurrences: function(arr, target) {
-
-  }
+    findAllOccurrences: function(arr, target) {
+        var result = [];
+        for (i=0; i<arr.length; i++){
+            if (arr[i]==target){
+                result.push(i);
+            }
+        }
+        return result
+    }
 };
--- tests/app/arrays.js
+++ tests/app/arrays.js
@@ -92,9 +92,9 @@ describe('arrays', function() {
   });
 
   it('you should be able to find duplicates in an array', function() {
-    var result = arraysAnswers.duplicates([ 1, 2, 4, 4, 3, 3, 1, 5, 3 ]);
+    var result = arraysAnswers.duplicates([ 1, 2, 4, 4, 3, 3, 1, 5, 3, 7, 9, 7, 7, 7, 7 ]);
 
-    expect(result.sort()).to.eql([1, 3, 4]);
+    expect(result.sort()).to.eql([1, 3, 4, 7]);
   });
 
   it('you should be able to square each number in an array', function() {
